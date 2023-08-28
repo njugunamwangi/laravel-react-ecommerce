751f51539b2fd756325ce5192b6088faad2563b6
njugunamwangi <njugunamwangi.n@gmail.com>
2023-08-28 23:11:15 +0300

    Handle creation of categories

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+use App\Http\Resources\CategoryResource;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return CategoryResource::collection(
+            Category::query()
+                ->orderBy('category', 'asc')
+                ->paginate(10)
+        );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCategoryRequest $request)
+    {
+        $data = $request->validated();
+
+        $category = Category::create($data);
+
+        return new CategoryResource($category);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Category $category)
+    {
+        return new CategoryResource($category);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCategoryRequest $request, Category $category)
+    {
+        $data = $request->validated();
+
+        $category->update($data);
+
+        return new CategoryResource($category);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+
+        return response('', 204);
+    }
+}
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Category extends Model
 {
-    use HasFactory;
+    use HasFactory, HasSlug ;
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('category')
+            ->saveSlugsTo('slug');
+    }
+
+    protected $fillable = [
+        'category',
+        'slug'
+    ];
+
+    public function products() : BelongsToMany {
+        return $this->belongsToMany(Product::class);
+    }
 }
